d1a17f6e07c301c8cead8d14cf6c6b03f68f2048
Anthony Villano <villaa@physics.umn.edu>
2016-06-15 19:21:25 -0500

    [ANV] added some code to reduce data

--- condor/submit.sh
+++ condor/submit.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 #step by 10 cm in thickness and source distance from the side try 100M primaries
-./condork100Sim_GenericShielding -n 100 -src PuBe -set 1 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -70.0 0.0 0.0 -shieldsize 20.0 20.0 40.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 100 -src PuBe -set 1 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -70.0 0.0 0.0 -shieldsize 20.0 20.0 40.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 100 -src PuBe -set 2 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -60.0 0.0 0.0 -shieldsize 20.0 20.0 30.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 100 -src PuBe -set 3 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -50.0 0.0 0.0 -shieldsize 20.0 20.0 20.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 100 -src PuBe -set 4 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -80.0 0.0 0.0 -shieldsize 20.0 20.0 50.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 100 -src PuBe -set 5 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -90.0 0.0 0.0 -shieldsize 20.0 20.0 60.0 -shieldmat Poly -exe
+./condork100Sim_GenericShielding -n 100 -src PuBe -set 6 -ngen 1000000 -template templates/pube.mac.template -merge -sourcepos -100.0 0.0 0.0 -shieldsize 20.0 20.0 70.0 -shieldmat Poly -exe
---
+++ sima2root/makeListsCondor_PuBe.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+id=0x0003
+src=PuBe
+simtype=GenericShield
+directory=captureCal
+nprimaries=1M
+
+        #create the .dat file for the chain object
+        sed -e "s|IDENTIFIER|${id}|" -e "s|SOURCE|${src}|" -e "s|DIRECTORY|${directory}|" -e "s|SIMTYPE|${simtype}|" -e "s|NPRIMARIES|${nprimaries}|" < PuBe.dat.template > ${directory}_${simtype}_${src}_${id}_${nprimaries}.dat 
+
+        #move .dat file to appropriate directory
+        mv ${directory}_${simtype}_${src}_${id}_${nprimaries}.dat lists/
+
+        #create corresponding .list file for the chain object
+	ls /data/chocula/villaa/k100Sim_Data/${directory}/${simtype}_${src}_${id}_${nprimaries}_* |awk 'BEGIN{FS="[._]"}{print $6"\t"$7"\t"$8"\t1";}'  > lists/${directory}_${simtype}_${src}_${id}_${nprimaries}.list
+
+	#get the date and make logfile names
+        DATE=`date +%y%m%d%H%M%S`
+
+        #create a condor file for the execution of the job
+	ARGUMENTS="-in lists/${directory}_${simtype}_${src}_${id}_${nprimaries}.dat -o ${simtype}_${src}_${id}_${nprimaries} -dir /data/chocula/villaa/k100Sim_Data/${directory}root/ -E" 
+	OUTPUTFILE=${directory}_${simtype}_${src}_${id}_${nprimaries}_${DATE}.txt
+	ERRORFILE=${directory}_${simtype}_${src}_${id}_${nprimaries}_${DATE}.err
+	SEDARG="s|THEARGUMENTS|${ARGUMENTS}|"
+
+	LOGFILE=${directory}_${simtype}_${source}_${id}_${nprimaries}_${DATE}.log
+
+
+        sed -e "${SEDARG}" -e "s|ERRORFILE|${ERRORFILE}|" -e "s|OUTPUTFILE|${OUTPUTFILE}|" -e "s|RUNLOGFILE|${LOGFILE}|" < condor/condor-SimA2Root  > .condorfile_${DATE}
+        
+        #cat .condorfile_${DATE}
+        condor_submit .condorfile_${DATE}
+	rm .condorfile_${DATE}
+	
