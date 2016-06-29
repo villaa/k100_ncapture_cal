a08cff637bdca7e737bc7532249e173f213413f8
Anthony Villano <villaa@physics.umn.edu>
2016-06-29 13:38:46 -0500

    [ANV] updated some code

--- skim/skimCode/skimData.cpp
+++ skim/skimCode/skimData.cpp
@@ -29,7 +29,6 @@
 
 using namespace std;
 
-TChain *chainSimData(int &nev,int n=-1,string sim="GenericShield", string source="PuBe", string nprimaries="10M",int datasetno=0,string dir="/data/chocula/villaa/k100Sim_Data/captureCalroot/");
 TTree *projectSumDep(TChain *ch,string cut="");
 TTree *projectSumDepEv(TChain *ch,string cut="");
 int main(int argc, char *argv[])
@@ -61,6 +60,10 @@ int main(int argc, char *argv[])
         datasetno = atoi(argv[i+1]);
       if(rep=="-source")
 	source = argv[i+1];
+      if(rep=="-outdir")
+	outdir = argv[i+1];
+      if(rep=="-nprim")
+	nprimaries = argv[i+1];
    }
 
    //get data set number string in hex
@@ -116,45 +119,6 @@ int main(int argc, char *argv[])
    cout << "Finished writing file. " << endl;
    return 0;
 }
-TChain *chainSimData(int &nev,int n,string sim, string source, string nprimaries,int datasetno,string dir)
-{
-   //get data set number string in hex
-   string dataset="";
-   std::ostringstream oss;
-   oss << std::hex << "0x" << std::setfill('0') << std::setw(4) << datasetno;
-   dataset = oss.str(); 
-
-  //create a chain
-  TChain *data = new TChain("datatree","datatree");
-
-  //make a command to use with a root pipe
-  string command = "ls "+dir+" |grep .root |grep "+sim+"_ |grep "+source+" |grep "+nprimaries+" |grep "+dataset;
-  TString files = gSystem->GetFromPipe(command.c_str());
-  string files_c = files.Data();
-  istringstream iss(files_c);
-
-  //get the files from the string
-  string filename;
-
-  //make a counter
-  int count=0;
-
-  while(!iss.eof()){
-    iss >> filename;
-    if(n<0 || count<n){
-      string fullfilename = dir+filename;
-      data->Add(fullfilename.c_str());
-    }
-    iss >> filename;
-    count++;
-  }
-  if(n>=0)
-    cout << "Chained " << n << " files" << endl;
-  else
-    cout << "Chained " << count << " files" << endl;
-
-  return data;
-}
 TTree *projectSumDep(TChain *ch,string cut)
 {
   Int_t nev;
@@ -186,9 +150,14 @@ TTree *projectSumDep(TChain *ch,string cut)
     ev[i] = tempev[i];
   }
 
+  //modify for coincidence detector 
+  //FIXME hard-code 1 for standard dectector 7 for ancillary detector
+  Double_t det0=1,detCoin=7;
+
   //make new TTree with total edepNR, edepER, nhit, NRhit, ERhit, positions?
   Long64_t nhit,NRhit,ERhit;
   Double_t edepNR,edepER;
+  Double_t edepCoin;
    
   // create tree and the ROOT file to store it
   TTree *datatree = new TTree("edeptree","energy deposition data");
@@ -199,6 +168,7 @@ TTree *projectSumDep(TChain *ch,string cut)
   datatree->Branch("ERhit",&ERhit,"ERhit/L");
   datatree->Branch("edepNR",&edepNR,"edepNR/D");
   datatree->Branch("edepER",&edepER,"edepER/D");
+  datatree->Branch("edepCoin",&edepCoin,"edepCoin/D");
 
   //loop through the events and make many TEventLists
   //taking care to make sure count unique events only
@@ -211,27 +181,33 @@ TTree *projectSumDep(TChain *ch,string cut)
       //evlist = new TEventList("evlist","event list");
       nhit=0;NRhit=0;ERhit=0;
       edepNR=0;edepER=0;
-      ch->Draw("type:d3",Form("seed==%d && ev==%d && d3>0.0 && Entry$>=%d",(Int_t)seed[i],(Int_t)ev[i],i),"goff");
+      ch->Draw("type:d3:dt",Form("seed==%d && ev==%d && d3>0.0 && Entry$>=%d",(Int_t)seed[i],(Int_t)ev[i],i),"goff");
 
       //get the list of useful hits
       Long_t htotsub=0;
       Double_t *typesub;
       Double_t *d3sub;
+      Double_t *detsub;
       htotsub = ch->GetSelectedRows();
       typesub = ch->GetV1();
       d3sub = ch->GetV2();
+      detsub = ch->GetV3();
       for(Int_t j=0;j<htotsub;j++){
-        if(((Long64_t)typesub[j] == 11 || (Long64_t)typesub[j] == -11) || (Long64_t)typesub[j] == 22){
+        if((((Long64_t)typesub[j] == 11 || (Long64_t)typesub[j] == -11) || (Long64_t)typesub[j] == 22) && (Long64_t)detsub[j]==1){
           //got ER hit
 	  edepER+=d3sub[j];
 	  ERhit++;
 	  nhit++;
 	}
-	else if((((Long64_t)typesub[j] -(Long64_t)typesub[j]%10000) > 1) || (Long64_t)typesub[j] == 2112){
+	else if(((((Long64_t)typesub[j] -(Long64_t)typesub[j]%10000) > 1) || (Long64_t)typesub[j] == 2112) && (Long64_t)detsub[j]==1){
           //got NR hit
 	  edepNR+=d3sub[j];
 	  NRhit++;
 	  nhit++;
+	}
+	else if((((Long64_t)typesub[j] == 11 || (Long64_t)typesub[j] == -11) || (Long64_t)typesub[j] == 22) && (Long64_t)detsub[j]==7){
+          //got ER hit
+	  edepCoin+=d3sub[j];
 	}
 	else{
 	  //what is this?
@@ -265,42 +241,92 @@ TTree *projectSumDepEv(TChain *ch,string cut)
   string ERcut = "(type==11 || type==-11) || type==22";
   string NRcut = "type==2112 || type-type%10000>1";
 
+  //modify for coincidence detector 
+  //FIXME hard-code 1 for standard dectector 7 for ancillary detector
+  Double_t det0=1,detCoin=7;
+
   //make new TTree with total edepNR, edepER, nhit, NRhit, ERhit, positions?
   Long64_t nhit,NRhit,ERhit;
+  Long64_t Ev,coinEv;
+  Long64_t seed;
   Double_t edepNR,edepER;
+  Double_t edepCoin;
    
   // create tree and the ROOT file to store it
   TTree *datatree = new TTree("edeptree","energy deposition data");
    
   // create branches in tree for each data column
   datatree->Branch("nhit",&nhit,"nhit/L");
+  datatree->Branch("seed",&seed,"seed/L");
+  datatree->Branch("Ev",&Ev,"Ev/L");
+  datatree->Branch("coinEv",&coinEv,"coinEv/L");
   datatree->Branch("NRhit",&NRhit,"NRhit/L");
   datatree->Branch("ERhit",&ERhit,"ERhit/L");
   datatree->Branch("edepNR",&edepNR,"edepNR/D");
   datatree->Branch("edepER",&edepER,"edepER/D");
+  datatree->Branch("edepCoin",&edepCoin,"edepCoin/D");
 
   //get meaningful quantities from the tree/chain
-  string NRhitstr = "Sum$(1*(("+NRcut+") && ("+cut+")))";
-  string ERhitstr = "Sum$(1*(("+ERcut+") && ("+cut+")))";
-  string edepNRstr = "Sum$(d3*(("+NRcut+") && ("+cut+")))";
-  string edepERstr = "Sum$(d3*(("+ERcut+") && ("+cut+")))";
-  ch->Draw(Form("%s:%s:%s:%s",NRhitstr.c_str(),ERhitstr.c_str(),edepNRstr.c_str(),edepERstr.c_str()),"","goff");
+  string NRhitstr = "Sum$(1*(("+NRcut+") && (dt==1) && ("+cut+")))";
+  string ERhitstr = "Sum$(1*(("+ERcut+") && (dt==1) && ("+cut+")))";
+  string edepNRstr = "Sum$(d3*(("+NRcut+") && (dt==1) && ("+cut+")))";
+  string edepERstr = "Sum$(d3*(("+ERcut+") && (dt==1) && ("+cut+")))";
+  string edepCoinstr = "Sum$(d3*(("+ERcut+") && (dt==7) && ("+cut+")))";
+  ch->Draw(Form("%s:%s:%s:ev",NRhitstr.c_str(),ERhitstr.c_str(),edepNRstr.c_str()),"","goff");
   Long64_t nevents = ch->GetSelectedRows();
+  cout << "selected: " << nevents << endl;
   Double_t *NRhitvec = ch->GetV1();
   Double_t *ERhitvec = ch->GetV2();
   Double_t *edepNRvec = ch->GetV3();
-  Double_t *edepERvec = ch->GetV4();
+  Double_t *ev = ch->GetV4();
+
+  Double_t *storeNRhitvec = (Double_t*)malloc(nevents*sizeof(Double_t));
+  Double_t *storeERhitvec = (Double_t*)malloc(nevents*sizeof(Double_t));
+  Double_t *storeedepNRvec = (Double_t*)malloc(nevents*sizeof(Double_t));
+  Double_t *storeedepERvec = (Double_t*)malloc(nevents*sizeof(Double_t));
+  Double_t *storeedepCoinvec = (Double_t*)malloc(nevents*sizeof(Double_t));
+  Long64_t *storeev = (Long64_t*)malloc(nevents*sizeof(Long64_t));
+  Long64_t *storeevCoin = (Long64_t*)malloc(nevents*sizeof(Long64_t));
+  Long64_t *storeevseed = (Long64_t*)malloc(nevents*sizeof(Long64_t));
+
+  for(int i=0;i<nevents;i++){
+    storeNRhitvec[i] = NRhitvec[i];
+    storeERhitvec[i] = ERhitvec[i];
+    storeedepNRvec[i] = edepNRvec[i];
+    storeev[i] = (Long64_t)ev[i];
+  }
+
+
+  ch->Draw(Form("%s:%s:ev:seed",edepCoinstr.c_str(),edepERstr.c_str()),"","goff");
+  nevents = ch->GetSelectedRows();
+  cout << "Reselected: " << nevents << endl;
+  Double_t *edepCoinvec = ch->GetV1();
+  Double_t *edepERvec = ch->GetV2();
+  Double_t *evCoin = ch->GetV3();
+  Double_t *evseed = ch->GetV4();
+
+  for(int i=0;i<nevents;i++){
+    storeedepCoinvec[i] = edepCoinvec[i];
+    storeedepERvec[i] = edepERvec[i];
+    storeevCoin[i] = (Long64_t) evCoin[i];
+    storeevseed[i] = (Long64_t) evseed[i];
+  }
 
   //loop through the lists and fill the tree;
   for(Long_t i=0; i<nevents; i++){
+    //cout << "Computing event: " << i << endl;
     nhit=0;NRhit=0;ERhit=0;
-    edepNR=0;edepER=0;
+    edepNR=0;edepER=0;edepCoin=0;
 
-    NRhit=NRhitvec[i];
-    ERhit=ERhitvec[i];
-    edepNR=edepNRvec[i];
-    edepER=edepERvec[i];
+    NRhit=storeNRhitvec[i];
+    ERhit=storeERhitvec[i];
+    edepNR=storeedepNRvec[i];
+    edepER=storeedepERvec[i];
+    edepCoin=storeedepCoinvec[i];
     nhit=NRhit+ERhit;
+    Ev = storeev[i];
+    coinEv = storeevCoin[i];
+    seed = storeevseed[i];
     datatree->Fill();
   }
 
