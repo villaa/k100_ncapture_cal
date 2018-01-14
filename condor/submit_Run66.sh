67d18abff06734192d714c20945768edbc558500
Anthony Villano <villaa@physics.umn.edu>
2018-01-13 20:50:12 -0600

    [ANV] updated submission scripts

--- condor/submit_Run66.sh
+++ condor/submit_Run66.sh
@@ -4,6 +4,16 @@
 #parameters for shieldmat and shieldsize are irrelevant because the
 #template doesn't use the generic shield setup
 #./condork100Sim_GenericShielding -n 1 -src PuBe -set 0 -ngen 10000000 -template templates/Run66ShieldTest.mac.template -merge -sourcepos 0.0 119.731 -56.515 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
-./condork100Sim_GenericShielding -n 10 -src PuBe -set 1 -ngen 1000000 -template templates/Run66Shield_stdLocation_inBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
-./condork100Sim_GenericShielding -n 10 -src PuBe -set 2 -ngen 1000000 -template templates/Run66Shield_stdLocation_outBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 10 -src PuBe -set 1 -ngen 1000000 -template templates/Run66Shield_stdLocation_inBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+#./condork100Sim_GenericShielding -n 10 -src PuBe -set 2 -ngen 1000000 -template templates/Run66Shield_stdLocation_outBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
 #./condork100Sim_GenericShielding -n 10 -src PuBe -set 3 -ngen 1000000 -template templates/Run66Shield_nearCryostat.mac.template -merge -sourcepos 23.348 23.348 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+
+
+
+#standard R66 config, neutrons from PuBe in barrel
+#Simulation 2 has increased Poly density
+#parameters for shieldmat and shieldsize are irrelevant because the
+#template doesn't use the generic shield setup
+./condork100Sim_GenericShielding -n 10 -src PuBe -set 1 -ngen 1000000 -template templates/Run66Sim2Shield_stdLocation_inBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+./condork100Sim_GenericShielding -n 10 -src PuBe -set 2 -ngen 1000000 -template templates/Run66Sim2Shield_stdLocation_outBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+./condork100Sim_GenericShielding -n 10 -src PuBe -set 3 -ngen 1000000 -template templates/Run66Sim2Shield_nearCryostat.mac.template -merge -sourcepos 23.348 23.348 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
--- condor/submit_Run66_Scans.sh
+++ condor/submit_Run66_Scans.sh
@@ -4,15 +4,19 @@
 #scan "high-flux" sim vertically and radially by 1cm  increments
 #vertical scan
 COUNTER=0
-for i in -5.0 -4.0 -3.0 -2.0 -1.0 1.0 2.0 3.0 4.0 5.0 
+for i in -10.0 -9.0 -8.0 -7.0 -6.0 -5.0 -4.0 -3.0 -2.0 -1.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0
 do
-  ./condork100Sim_GenericShielding -n 1 -src PuBe -set ${COUNTER} -ngen 1000000 -template templates/Run66Shield_nearCryostat_Rscan.mac.template -merge -sourcepos 23.348 23.348 ${i} -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+  if (( $(echo "($i == -6.0) || ($i==6.0)" | bc -l) )); then
+  ./condork100Sim_GenericShielding -n 1 -src PuBe -set ${COUNTER} -ngen 1000000 -template templates/Run66Shield_nearCryostat_Vscan.mac.template -merge -sourcepos 23.348 23.348 ${i} -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+  fi
   let COUNTER++
 done
 #radial scan (calculated to shift by 1cm increments radially)
 COUNTER=0
 for i in 20.520 21.227 21.934 22.641 24.055 24.7622 25.469 26.176 
 do
-  ./condork100Sim_GenericShielding -n 1 -src PuBe -set ${COUNTER} -ngen 1000000 -template templates/Run66Shield_nearCryostat_Vscan.mac.template -merge -sourcepos ${i} ${i} 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+  if (( $(echo "($i == 20.520)" | bc -l) )); then
+  ./condork100Sim_GenericShielding -n 1 -src PuBe -set ${COUNTER} -ngen 1000000 -template templates/Run66Shield_nearCryostat_Rscan.mac.template -merge -sourcepos ${i} ${i} 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+  fi
   let COUNTER++
 done
