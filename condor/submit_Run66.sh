a4252d21e764aba8f0b21cca59bfcc36b986b395
Anthony Villano <villaa@physics.umn.edu>
2018-01-18 14:14:27 -0600

    [ANV] udpated

--- condor/submit_Run66.sh
+++ condor/submit_Run66.sh
@@ -32,4 +32,4 @@
 #template doesn't use the generic shield setup
 ./condork100Sim_GenericShielding -n 60 -src PuBe -set 1 -ngen 1000000 -template templates/Run66Shield_stdLocation_inBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -tagname Sim4 -exe
 ./condork100Sim_GenericShielding -n 40 -src PuBe -set 2 -ngen 1000000 -template templates/Run66Shield_stdLocation_outBarrel.mac.template -merge -sourcepos 0.0 119.731 -64.679 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -tagname Sim4 -exe
-./condork100Sim_GenericShielding -n 1 -src PuBe -set 3 -ngen 1000000 -template templates/Run66Shield_nearCryostat.mac.template -merge -sourcepos 23.348 23.348 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -tagname Sim4 -exe
+./condork100Sim_GenericShielding -n 10 -src PuBe -set 3 -ngen 1000000 -template templates/Run66Shield_nearCryostat.mac.template -merge -sourcepos 23.348 23.348 0.0 -shieldsize 40.0 40.0 40.0 -shieldmat Poly -tagname Sim4 -exe
