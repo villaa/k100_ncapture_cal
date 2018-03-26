c16432e196c5cbb6fb28fc046178119e0539078a
Anthony Villano <villaa@physics.umn.edu>
2018-03-26 00:46:54 -0500

    [ANV] added codes for different Flux Scans

---
+++ condor/organize_Run66_FluxScan1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#standard R66 config, neutrons from PuBe 
+#scan "high-flux" to "low-flux" situation along straight line 
+COUNTER=0
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+  DATASETIDHEX=`printf "0x%04x" ${COUNTER}`
+  rad=`./getPositionScan_HtoL.py -r ${i}`
+  num=`ls -lh /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldFluxScan1_PuBe_${DATASETIDHEX}_1M*000.txt |awk 'END{print NR;}'`
+  cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldFluxScan1_PuBe_${DATASETIDHEX}_1M* |awk '{if($22==1 && $2==1){print $1"\t"$2"\t"$22}}' |awk -v rad=${rad} -v offset=${i} -v num=${num} 'END{print rad"\t"NR"\t"num}'
+  let COUNTER++
+done
---
+++ condor/submit_Run66_FluxScan2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#standard R66 config, neutrons from PuBe 
+#scan "high-flux" to "low-flux" situation along straight line 
+COUNTER=0
+for i in 1 2 3 4 5 6 7 8 9 10 
+do
+  ./condork100Sim_GenericShielding -n 10 -src PuBe -set ${COUNTER} -ngen 1000000 -template templates/Run66Shield_FluxScan2.mac.template -merge -sourcepos `./getPositionScan_HtoL.py --n 10 ${i}` -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+  let COUNTER++
+done
