fe7396821babf2b1c3f8aefaabc6d922e4e20acc
Anthony Villano <anthony.villano@ucdenver.edu>
2018-06-17 15:09:38 -0500

    [ANV] added estimate with R68 thresholds

--- condor/organize_Run66_RateMeasure11_allRates.sh
+++ condor/organize_Run66_RateMeasure11_allRates.sh
@@ -11,6 +11,6 @@ do
   rateZIP=`cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldRateMeasure11_PuBe_${DATASETIDHEX}_1M*.txt |awk '{if($2==1 && $6>0.001){count[$1]++}}END{for(i in count){print i}} ' |awk 'END{print NR}'`
   rateNaIB=`cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldRateMeasure11_PuBe_${DATASETIDHEX}_1M*.txt |awk '{if($2==37 && $6>3.4){count[$1]++}}END{for(i in count){print i}} ' |awk 'END{print NR}'`
   rateNaIC=`cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldRateMeasure11_PuBe_${DATASETIDHEX}_1M*.txt |awk '{if($2==43 && $6>3.4){count[$1]++}}END{for(i in count){print i}} ' |awk 'END{print NR}'`
-  cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldRateMeasure8_PuBe_${DATASETIDHEX}_1M* |awk '{if($22==1 && $2==1){print $1"\t"$2"\t"$22}}' |awk -v rad=${rad} -v offset=${i} -v rateZIP=${rateZIP} -v rateNaIB=${rateNaIB} -v rateNaIC=${rateNaIC} -v num=${num} 'END{print rad"\t"NR"\t"rateZIP"\t"rateNaIB"\t"rateNaIC"\t"num}'
+  cat /data/chocula/villaa/k100Sim_Data/captureCal/Run66ShieldRateMeasure11_PuBe_${DATASETIDHEX}_1M* |awk '{if($22==1 && $2==1){print $1"\t"$2"\t"$22}}' |awk -v rad=${rad} -v offset=${i} -v rateZIP=${rateZIP} -v rateNaIB=${rateNaIB} -v rateNaIC=${rateNaIC} -v num=${num} 'END{print rad"\t"NR"\t"rateZIP"\t"rateNaIB"\t"rateNaIC"\t"num}'
   let COUNTER++
 done
