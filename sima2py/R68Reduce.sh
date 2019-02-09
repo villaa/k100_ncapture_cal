a8b08398fa726510e3351ab7eb631cc200057ff7
Anthony Villano <anthony.villano@ucdenver.edu>
2019-02-09 16:41:52 -0600

    [ANV] added a notebook to look at reduced R68 simulation data

--- sima2py/R68Reduce.sh
+++ sima2py/R68Reduce.sh
@@ -3,7 +3,7 @@
 #standard R68 config, neutrons from PuBe 
 TAG=$1
 #TAG='Run68_gdirect_bknd_R68_PuBe'
-QUANTITYREGEX='10M'
+QUANTITYREGEX='1M'
 DATASET=$2
 #DATASET=5
 DATASETIDHEX=`printf "0x%04x" ${DATASET}`
