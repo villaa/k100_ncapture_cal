fb638c35164e8c34d80df4bcd807fcc1d9e1526e
Anthony Villano <anthony.villano@ucdenver.edu>
2019-02-09 13:16:59 -0600

    [ANV] added new skimming

---
+++ sima2py/R68Reduce.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#standard R68 config, neutrons from PuBe 
+TAG=$1
+#TAG='Run68_gdirect_bknd_R68_PuBe'
+QUANTITYREGEX='1M'
+DATASET=$2
+#DATASET=5
+DATASETIDHEX=`printf "0x%04x" ${DATASET}`
+IDENTIFIER=$3
+
+ARGS='--filedir=/data/chocula/villaa/k100Sim_Data/captureCal/ --regex='${TAG}_${DATASETIDHEX}_${QUANTITYREGEX}_${IDENTIFIER}\\S+' --outfile='/data/chocula/villaa/k100Sim_Data/captureCalhdf5/${TAG}_${DATASETIDHEX}_${i}.h5''
+echo ${ARGS}
+./sima2py.py ${ARGS}
---
+++ sima2py/submit_R68Reduce.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#standard R68 config, neutrons from PuBe 
+
+TAG=$1
+DATASET=$2
+DATASETIDHEX=`printf "0x%04x" ${DATASET}`
+LIST=`ls /data/chocula/villaa/k100Sim_Data/captureCal/${TAG}_${DATASETIDHEX}* |awk 'BEGIN{FS="_"}{print $9;}'|uniq`
+
+for i in ${LIST} 
+do
+  ARGS=${TAG}' '${DATASET}' '${i}
+  LOGPREFIX=${TAG}_${DATASETIDHEX}_${i}
+  #echo ${ARGS}
+  #echo ${LOGPREFIX}
+  sed -e 's|ARGS|'"${ARGS}"'|g' -e 's|LOGPREFIX|'${LOGPREFIX}'|g' < condor-k100R68SimReduce > .condor
+  cat .condor
+  #condor_submit .condor
+  rm .condor
+done
