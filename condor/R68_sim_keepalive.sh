193d8ce80bbcba3f5de08eeed213779a14418948
Anthony Villano <anthony.villano@ucdenver.edu>
2019-05-03 17:58:31 -0500

    [ANV] updated scripts for submitting R68 jobs

--- condor/R68_sim_keepalive.sh
+++ condor/R68_sim_keepalive.sh
@@ -10,8 +10,8 @@ CDMSload=`condor_q -wide |grep genShieldSimCondense.sh|awk '{print $9}'`
 echo "CMS load: "${CMSload} 
 echo "CDMS load: "${CDMSload} 
 
-CMSmax=10
-CDMSmax=10
+CMSmax=600
+CDMSmax=100
 
 CMSsubmit=$((${CMSmax}-${CMSload}))
 CDMSsubmit=$((${CDMSmax}-${CDMSload}))
@@ -21,3 +21,5 @@ echo "CDMS will submit: "${CDMSsubmit}
 
 ./condork100Sim_GenericShielding_CMS -n ${CMSsubmit} -src R68_PuBe -set 6 -ngen 10000000 -template templates/Run68_gdirect_bknd.mac.template -merge -sourcepos `./getPositionScan_HtoL.py --n 10 10` -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
 ./condork100Sim_GenericShielding -n ${CDMSsubmit} -src R68_PuBe -set 6 -ngen 10000000 -template templates/Run68_gdirect_bknd.mac.template -merge -sourcepos `./getPositionScan_HtoL.py --n 10 10` -shieldsize 40.0 40.0 40.0 -shieldmat Poly -exe
+
+date
